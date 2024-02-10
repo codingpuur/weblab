39a1c1faf2e72828566705e4bbe82c4ea8da433d
shubhwebdev <sc5112492@gmail.com>
2024-02-10 17:33:02 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+"use client"
 import Image from "next/image";
 import Navbar from "@/components/navbar/Navbar";
 import heroImage from "@/public/assest/hero.jpeg";
 import servicesImage from "@/public/assest/services.jpeg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 import logo from "@/app/favicon.ico";
+import Autoplay from 'embla-carousel-autoplay'
+
+
 import { useEffect } from "react";
 
 export default function Home() {
@@ -16,20 +27,19 @@ export default function Home() {
     <main className="flex gap-10 min-h-screen flex-col items-center  px-5 lg:px-24 pt-2">
       <Navbar />
 
-      <div className=" flex gap-10 h-full  relative flex-col md:flex-row ">
-        <div className=" relative ">
-          <Image className="" src={heroImage} alt="Next.js Logo" priority />
-          <div className="  absolute  top-0 py-16 flex  flex-col h-full  text-[3rem]   lg:text-[7rem]   font-bold pl-10   leading-none uppercase">
-            <p>Let&apos;s build the next</p>
-            <div className=" absolute bottom-5 text-sm flex justify-between px-16 left-0   border-t-2 w-full">
-              <div>one</div>
-              <div>two</div>
-            </div>
-          </div>
-          <div></div>
+      <div className=" flex gap-5 h-full  relative flex-col md:flex-row ">
+    
+   
+       
+         
+          <div className="    top-0  flex  flex-col h-full  text-[3rem]   lg:text-[7rem]   font-bold    leading-none uppercase">
+          <video src="https://d3vlq52qrgdnc2.cloudfront.net/Services-Showreel-1080.mp4" className=" object-cover rounded-2xl  w-full h-full"   autoPlay loop muted />
+        
+        
+         
         </div>
         <div>
-          <div className=" bg-white/10 h-full rounded-3xl w-[27rem] p-5">
+          <div className=" bg-white/10 h-full rounded-3xl  w-full md:w-[24rem] p-5">
             <div className=" flex  justify-between items-center">
               <div className=" flex justify-center items-center gap-2 ">
                 <div>
@@ -51,6 +61,43 @@ export default function Home() {
               Next.js is a JavaScript framework for building single-page
               applications (SPAs).
             </p>
+            <div>
+            <Carousel
+             plugins={[
+              Autoplay({
+                delay: 5000,
+              }),
+            ]}
+            
+            >
+  <CarouselContent>
+    <CarouselItem>
+      <Image
+        src={heroImage}
+        alt="Next.js Logo"
+        priority
+      />
+    </CarouselItem>
+    <CarouselItem>
+      <Image
+        src={heroImage}
+        alt="Next.js Logo"
+        priority
+      />
+    </CarouselItem>
+    <CarouselItem>
+      <Image
+        src={heroImage}
+        alt="Next.js Logo"
+        priority
+      />
+    </CarouselItem>
+   
+  </CarouselContent>
+ 
+  <CarouselNext />
+</Carousel>
+            </div>
           </div>
         </div>
       </div>
@@ -58,58 +105,58 @@ export default function Home() {
 
 
 
-      <div className="services relative h-fit">
+      {/* <div className="services relative h-fit">
          <Image src={servicesImage} alt="services image"/>
          <div className="absolute top-5 w-full flex flex-col gap-10">
           <div className=" top-bar flex   text-black  justify-between w-full px-6">
-            <h1 className=" md:text-9xl   md:leading-[7rem]   font-[500] w-1/2">OUR SERVICES</h1>
+            <h1 className=" text-9xl   leading-[7rem]   font-[500] w-1/2">OUR SERVICES</h1>
             <div className=" w-1/2  flex  justify-end  items-end   ">
-              <div className=" hidden text-4xl bg-black rounded-full p-10 text-white ">logo</div>
+              <div className=" text-4xl bg-black rounded-full p-10 text-white ">logo</div>
             </div>
           </div>
-          <div className="bottom   w-full h-full grid md:grid-cols-2 gap-5 text-black md:p-10 ">
-            <div className=" border-2 flex md:gap-16  md:p-10  bg-gray-100 rounded-3xl ">
-              <div className=" hidden md:flex  justify-start h-full items-start ">
+          <div className="bottom   w-full h-full grid grid-cols-2 gap-5 text-black p-10 ">
+            <div className=" border-2 flex gap-16  p-10  bg-gray-100 rounded-3xl ">
+              <div className=" flex  justify-start h-full items-start ">
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="120" height="120"/>
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="100" height="100"/>
               
               </div>
               <div>
-                <h1 className=" md:leading-8 md:text-4xl font-semibold">Product design </h1>
-                <p className=" hidden md:block py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
+                <h1 className=" leading-8 text-4xl font-semibold">Product design </h1>
+                <p className=" py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
               </div>
                </div>
-            <div className=" border-2 flex md:gap-16  md:p-10  bg-gray-100 rounded-3xl ">
-              <div className=" hidden md:flex  justify-start h-full items-start ">
+            <div className=" border-2 flex gap-16  p-10  bg-gray-100 rounded-3xl ">
+              <div className=" flex  justify-start h-full items-start ">
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="120" height="120"/>
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="100" height="100"/>
               
               </div>
               <div>
-                <h1 className=" md:leading-8 md:text-4xl font-semibold">Product design </h1>
-                <p className=" hidden md:block py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
+                <h1 className=" leading-8 text-4xl font-semibold">Product design </h1>
+                <p className=" py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
               </div>
                </div>
-            <div className=" border-2 flex md:gap-16  md:p-10  bg-gray-100 rounded-3xl ">
-              <div className=" hidden md:flex  justify-start h-full items-start ">
+            <div className=" border-2 flex gap-16  p-10  bg-gray-100 rounded-3xl ">
+              <div className=" flex  justify-start h-full items-start ">
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="120" height="120"/>
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="100" height="100"/>
               
               </div>
               <div>
-                <h1 className=" md:leading-8 md:text-4xl font-semibold">Product design </h1>
-                <p className=" hidden md:block py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
+                <h1 className=" leading-8 text-4xl font-semibold">Product design </h1>
+                <p className=" py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
               </div>
                </div>
-            <div className=" border-2 flex md:gap-16  md:p-10  bg-gray-100 rounded-3xl ">
-              <div className=" hidden md:flex  justify-start h-full items-start ">
+            <div className=" border-2 flex gap-16  p-10  bg-gray-100 rounded-3xl ">
+              <div className=" flex  justify-start h-full items-start ">
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="120" height="120"/>
                 <Image src="https://assets-global.website-files.com/63f38a8c92397a024fcb9ae8/64f852beac4d6ff134432f2b_icon-services-research.svg" alt="" width="100" height="100"/>
               
               </div>
               <div>
-                <h1 className=" md:leading-8 md:text-4xl font-semibold">Product design </h1>
-                <p className=" hidden md:block py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
+                <h1 className=" leading-8 text-4xl font-semibold">Product design </h1>
+                <p className=" py-5 text-xl">By putting users&apos; needs at the forefront, we tell a unique story of your company, juggling with fancy visual elements.</p>
               </div>
                </div>
            
@@ -119,7 +166,7 @@ export default function Home() {
 
 
 
-      </div>
+      </div> */}
     </main>
   );
 }
